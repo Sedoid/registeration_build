8070a628a09e66fcd0ef9ea44b5b5907651fea30
sedoid <karlsedoide@gmail.com>
2020-04-03 01:44:51 +0100

    enabled cors

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const accounts = {
     accounts: []
 }
 
-
+app.use((req,res,next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+})
 
 fs.mkdir("database",() =>{
     console.log(" Directory Succesfully Created");
